81ba2de3c01f13d181c983fab8f0d9f3995f40ab
domingasp <d.pleck@outlook.com>
2024-09-29 20:37:24 +0100

    feat: global budget

---
+++ subscription-budget.tf
@@ -0,0 +1,23 @@
+data "azurerm_subscription" "current" {}
+
+resource "azurerm_consumption_budget_subscription" "global_budget" {
+  name            = "global_subscription_budget"
+  subscription_id = data.azurerm_subscription.current.id
+
+  amount     = 10
+  time_grain = "Monthly"
+
+  time_period {
+    start_date = "2024-09-01T00:00:00Z"
+  }
+
+  notification {
+    enabled   = true
+    threshold = 60.0
+    operator  = "EqualTo"
+
+    contact_emails = [
+      "hello@domingasp.com"
+    ]
+  }
+}
